09eaa835de68006927296850044784f4aa3641f3
Kevin Choi <choi1june88@gmail.com>
2023-07-20 17:03:33 -0700

    get / working

--- test/spec.js
+++ test/spec.js
@@ -1,6 +1,16 @@
 const { expect } = require("chai");
+const { describe } = require("mocha");
+const request = require("supertest")(require("./app"));
 
 //This is test suite
 it("true is true", () => {
   expect(true).to.be.equal(true);
+});
+
+describe("GET /", () => {
+  it("should receive 200", async () => {
+    // request().get("/").expect(200);
+    const response = await request.get("/");
+    expect(response.status).to.equal(200);
+  });
 });
